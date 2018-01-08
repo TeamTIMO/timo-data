c6a5a6cd5a4fd3fb1ecb5a455483c7513862e9e4
Domink Sigmund <sigmund.dominik@googlemail.com>
2018-01-08 13:33:00 +0100

    Basic Coding Complete

--- README.md
+++ README.md
@@ -1,43 +1,58 @@
-TIMO - Data Microservice
-========================
+# TIMO - Data Microservice
 
 Simple CRUD Webservice, will manage the internal id-to-play database
 
-# Database
-Simple JSON File, hold in memory for faster reading  
-Example:
+## Database
+
+Simple JSON File, hold in memory for faster reading.
+
+__Example:__
+
 ```json
 [
-	{
-		"id":"rfid-id",
-		"source":"local",
-		"link":"/var/files/test.mp3"
-	},
-	...
+  {
+    "id":"ef5e5247-9cce-4de9-acc9-0d17b3850f94",
+    "source":"local",
+    "link":"/var/files/test.mp3"
+  },
+  ...
 ]
 ```
-## id
-UUID, used on the rfis cards
 
-## source
+### id
+
+UUID, used on the rfid cards
+
+### source
+
 Where is this file? Local, YOuTube, Spotify, ...
 
-## link
+### link
+
 Route to File, differs by source
 
-# API
-* GET /list <- return full json
+## API
+
+* GET / <- return full json
 * GET /:id <- return data json for id
 * GET /sources <- return all sources
 * POST / <- add new entry
 * PUT /:id <- update entry with id
 * DELETE /:id <- delete entry with id
 
-# TODO
+## TODO
+
 * Add raml for api
-* code routes
-* lib database
-* jsonschema
 * testcases
 * code coverage
-* build script (testresults into html)
+* puml class diagrams
+* puml sequence for routes and main
+* jsdoc
+* build stuff
+	* jsdoc
+	* tests
+	* code coverage
+	* raml
+	* puml
+	* json-schema
+	* add commandos and results to this file
--- app.js
+++ app.js
@@ -6,18 +6,24 @@
  * @description Starts the Server and keeps it running
  * @memberof timo-data
  * @requires express
+ * @requires lib/database
+ * @requires uuid
  */
 
  // Require needed modules
  console.log('Starting up TIMO-Dataservice...')
  console.log('Pulling in dependencies...')
+ var config = require('./config.json')
  /**
  * express module
  * @const
  */
-var express = require('express')
-var app = express()
-var server = require('http').createServer(app)
+ var express = require('express')
+ var app = express()
+ var server = require('http').createServer(app)
+ const uuid = require('uuid4')
+ var Database = require('database')
+ var myDB = new Database(config.dbfilelocation, uuid)
 
 // Listen to Port
  if (process.argv.length >= 4) {
@@ -27,8 +33,60 @@ var server = require('http').createServer(app)
  console.log('TIMO-Dataservice running on ' + config.port)
 
 // Routes
-
-// TODO: Add Routes
+ app.get('/', function (req, res) {
+   myDB.list(function (err, list) {
+     if (err) {
+       res.status(500).send(err)
+     } else {
+       res.send(list)
+     }
+   })
+ })
+ app.get('/sources', function (req, res) {
+   myDB.sources(function (err, sources) {
+     if (err) {
+       res.status(500).send(err)
+     } else {
+       res.send(sources)
+     }
+   })
+ })
+ app.get('/:id', function (req, res) {
+   myDB.get(req.params.id, function (err, entry) {
+     if (err) {
+       res.status(404).send(err)
+     } else {
+       res.send(entry)
+     }
+   })
+ })
+ app.post('/', function (req, res) {
+   myDB.add(req.body, function (err, entry) {
+     if (err) {
+       res.status(403).send(err)
+     } else {
+       res.send(entry)
+     }
+   })
+ })
+ app.put('/:id', function (req, res) {
+   myDB.update(req.params.id, req.body, function (err, entry) {
+     if (err) {
+       res.status(404).send(err)
+     } else {
+       res.send(entry)
+     }
+   })
+ })
+ app.delete('/:id', function (req, res) {
+   myDB.delete(req.params.id, function (err, entry) {
+     if (err) {
+       res.status(404).send(err)
+     } else {
+       res.send(true)
+     }
+   })
+ })
 
 /** Handles exitEvents by destroying open connections first
  * @function
@@ -49,4 +107,3 @@ var server = require('http').createServer(app)
 
   // keep running
  process.stdin.resume()
-
